27ab9fc47214bc21cbddb12689c42ba59b6571ec
lylynk <lyn1204kh@gmail.com>
2024-09-11 15:01:55 +0700

    test search/create

---
+++ Test/TestCreate/TestCreate/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TestCreate
+{
+    public class PostTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+         
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("http://localhost:3001/");
+        }
+
+        [Test]
+        public void CreatePost_ShouldDisplayPostOnPage_WhenPostIsValid()
+        {
+            
+            var usernameField = driver.FindElement(By.Id("username"));
+            var passwordField = driver.FindElement(By.Id("password"));
+            var loginButton = driver.FindElement(By.Id("loginButton"));
+
+            usernameField.SendKeys("your_username");
+            passwordField.SendKeys("your_password");
+            loginButton.Click();
+
+            // Điều hướng đến trang đăng bài
+            driver.Navigate().GoToUrl("http://localhost:3001/");
+
+            // Điền thông tin bài viết
+            var titleField = driver.FindElement(By.Id("postTitle"));
+            var contentField = driver.FindElement(By.Id("postContent"));
+            var submitButton = driver.FindElement(By.Id("submitPost"));
+
+            titleField.SendKeys("Bài viết mới từ Selenium");
+            contentField.SendKeys("Đây là nội dung của bài viết mới.");
+            submitButton.Click();
+
+            var confirmationMessage = driver.FindElement(By.ClassName("post-success-message"));
+
+            Assert.IsTrue(confirmationMessage.Displayed, "Bài viết đã được đăng thành công.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Đóng trình duyệt sau khi test kết thúc
+            driver.Quit();
+        }
+    }
+}
---
+++ Test/TestSearch/TestSearch/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestSearch
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Khởi tạo trình điều khiển Chrome
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:3001/");
+        }
+
+        [Test]
+        public void SearchDocument_Found_ReturnsCorrectResult()
+        {
+            IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
+            searchInput.SendKeys("Document1");
+
+            IWebElement searchButton = driver.FindElement(By.Id("searchButton"));
+            searchButton.Click();
+
+            IWebElement result = driver.FindElement(By.Id("result"));
+            Assert.IsTrue(result.Text.Contains("Document1"), "Document1 should be found.");
+        }
+
+        [Test]
+        public void SearchDocument_NotFound_ReturnsNotFoundMessage()
+        {
+            // Tìm ô tìm kiếm và nhập từ khóa không hợp lệ
+            IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
+            searchInput.SendKeys("UnknownDocument");
+
+            // Tìm và nhấn nút tìm kiếm
+            IWebElement searchButton = driver.FindElement(By.Id("searchButton"));
+            searchButton.Click();
+
+            // Đợi kết quả và kiểm tra nội dung
+            IWebElement result = driver.FindElement(By.Id("result"));
+            Assert.AreEqual("Not Found", result.Text, "The document should not be found.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
